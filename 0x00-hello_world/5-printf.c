990cb791c0a3619133e277384271c7edb879afa3
EPHRAIM4774 <ephikhomo@gmail.com>
2023-07-13 15:05:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * 
+ * main - A function that prints a line using printf functin
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n)
+return (0);
+}

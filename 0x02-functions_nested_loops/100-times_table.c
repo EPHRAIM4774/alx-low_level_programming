5b7c0914a69e86752d863c17f7cd61853bfbe22a
EPHRAIM4774 <ephikhomo@gmail.com>
2023-07-19 13:52:10 -0700

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * print_number - prints an integer
- * @n: inptu integer parameter
+ * @n: input integer parameter
  *
  * Return: Always 0
  */
-void print_number(int n)
+int print_number(int n)
 {
 	unsigned int i = n;
 
